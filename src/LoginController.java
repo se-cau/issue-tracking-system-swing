8ec89f05c947346b5c29c1d114e5c8725c12d42b
mininim <minsoplee2015@gmail.com>
2024-05-25 17:03:34 +0900

    feat: LoginController 구현
    Login 유효한지 확인
    RegisterController 연결

---
+++ src/LoginController.java
@@ -0,0 +1,72 @@
+import its.model.LoginRequest;
+import its.network.NetworkManager;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginController extends JFrame {
+    private JTextField useridTextField;
+    private JTextField passwordTextField;
+    private JButton loginButton;
+    private JPanel loginPanel;
+    private JLabel loginLabel;
+    private JButton registerButton;
+
+    public LoginController() {
+        setContentPane(loginPanel);
+        setTitle("Issue Tracking System");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(1000, 700);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        loginLabel.setFont(loginLabel.getFont().deriveFont(loginLabel.getFont().getSize() * 2.0f));
+
+        loginButton.addActionListener(loginButtonListener);
+        registerButton.addActionListener(registerButtonListener);
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new LoginController();
+    }
+
+    ActionListener loginButtonListener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Loginbutton 클릭");
+
+            String id = useridTextField.getText();
+            String password = passwordTextField.getText();
+
+            if (id.isEmpty() || password.isEmpty()) {
+                JOptionPane.showMessageDialog(null, "아이디/비밀번호를 올바르게 입력하세요");
+                return;
+            }
+
+            try {
+                NetworkManager.login(new LoginRequest(id, password));
+                System.out.println("로그인 성공");
+                //ToDo 로그인 성공 이후 화면 연결
+
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(null, ex.getMessage());
+            }
+        }
+    };
+
+    ActionListener registerButtonListener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Register 버튼 클릭");
+
+            new RegisterController();
+            dispose();
+
+        }
+    };
+
+
+
+}
